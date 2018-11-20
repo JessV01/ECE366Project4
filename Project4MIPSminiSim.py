7caef3f0bb44c82980d85a89ed91108e54ffb9b7
JessV01 <jv2@uic.edu>
2018-11-19 23:06:38 -0600

    Starting part2

--- Project4MIPSminiSim.py
+++ Project4MIPSminiSim.py
@@ -2,13 +2,12 @@
 # Created by: Jessica Vargas, Vaishnavi Medikundam
 # The program takes in MIPS instruction memory in hex and returns data
 
-def MIPSsim(HexInstr, BinInstr):
+def MIPSsim(HexInstr, BinInstr, Part2):
     size_of_mem = 1024                      # NOT SURE IF FOR SURE CORRECT
-    print("Beginning MIPS simulation...")
-    print("Info:")
+    print("-------------------------\nBeginning MIPS simulation...\n-------------------------")
     
     Reg = [0,0,0,0,0,0,0,0]                # the $0-$7 registers initialized to 0
-    Mem = [0 for i in range(size_of_mem)]   # creates memory arrary initialized to 0's
+    Mem = [0 for i in range(size_of_mem)]   # creates memory array initialized to 0's
     PC =0
     DIC = 0
     numCycles = 0
@@ -21,7 +20,8 @@ def MIPSsim(HexInstr, BinInstr):
         DIC += 1
         fetch = BinInstr[PC]
         if(fetch[0:32] == '00010000000000001111111111111111'):
-            print("deadloop")
+            if(Part2):
+                print("deadloop")
             end = True
             
         elif(fetch[0:6] == '000000'): # R-type instructions
@@ -32,17 +32,20 @@ def MIPSsim(HexInstr, BinInstr):
             if(fetch[26:32] == '100000'): # 'add'
                 PC += 1
                 Reg[Rd] = Reg[Rs] + Reg[Rt]
-                print("add " + str(Reg[Rd]))
+                if(Part2):
+                    print("add $"+str(Rd)+",$"+str(Rs)+",$"+str(Rt))
 
             elif(fetch[26:32] == '100010'): # 'sub'
                 PC += 1
                 Reg[Rd] = Reg[Rs] - Reg[Rt]
-                print("sub")
+                if(Part2):
+                    print("sub $"+str(Rd)+",$"+str(Rs)+",$"+str(Rt))
                 
             elif(fetch[26:32] == '100110'): # 'xor'
                 PC += 1
                 Reg[Rd] = Reg[Rs] ^ Reg[Rt]
-                print("xor")
+                if(Part2):
+                    print("xor $"+str(Rd)+",$"+str(Rs)+",$"+str(Rt))
                 
             elif(fetch[26:32] == '101010'): # 'slt'
                 PC += 1
@@ -50,7 +53,8 @@ def MIPSsim(HexInstr, BinInstr):
                     Reg[Rd] = 1
                 else:
                     Reg[Rd] = 0
-                print("slt")
+                if(Part2):
+                    print("slt $"+str(Rd)+",$"+str(Rs)+",$"+str(Rt))
             else:
                 end = True
                 
@@ -65,44 +69,58 @@ def MIPSsim(HexInstr, BinInstr):
             if(fetch[0:6] == '001000'): # 'addi'
                 PC += 1
                 Reg[Rt] = Reg[Rs] + Imm
-                print("addi: Rt="+str(Rt)+" Rs="+str(Rs)+" Imm="+str(Imm)+" sum="+str(Reg[Rt]))
+                if(Part2):
+                    print("addi $"+str(Rt)+",$"+str(Rs)+","+str(Imm))
 
             elif(fetch[0:6] == '000100'): # 'beq'
                 if(Reg[Rs] == Reg[Rt]):
-                    PC = PC + Imm + 1 #MIGHT BE WRONG (check if have to divide by 4??)
+                    PC = PC + Imm + 1 
                 else:
                     PC += 1
-                print("beq")
+                if(Part2):
+                    print("beq $"+str(Rs)+",$"+str(Rt)+","+str(Imm))
                 
             elif(fetch[0:6] == '000101'): # 'bne'
                 if(Reg[Rs] != Reg[Rt]):
-                    PC = PC + Imm + 1 #MIGHT BE WRONG
+                    PC = PC + Imm + 1 
                 else:
                     PC += 1
-                print("bne")
+                if(Part2):
+                    print("bne $"+str(Rs)+",$"+str(Rt)+","+str(Imm))
                 
             elif(fetch[0:6] == '100011'): # 'lw'
                 PC += 1
                 Reg[Rt] = Mem[((Reg[Rs] + Imm)-8192)]
-                print("lw")
+                if(Part2):
+                    print("lw $"+str(Rt)+","+str(Imm)+"($"+str(Rs)+")")
                 
             elif(fetch[0:6] == '101011'): # 'sw'
                 PC += 1
                 Mem[((Reg[Rs] + Imm)-8192)] = Reg[Rt]
-                print("sw ")
+                if(Part2):
+                    print("sw $"+str(Rt)+","+str(Imm)+"($"+str(Rs)+")")
                 
             else:
                 end = True
 
-    print("Sim done...results: \n")
+    print("-------------------------\nSimulation done...\n-------------------------")
+    print("*Results*:")
     print("Dynamic Instruction Count: " + str(DIC))
     print("PC: " + str(PC*4))
-    print("Registers: " + str(Reg))
+    print("Registers: ")
+    print("   $1 = " + str(Reg[1]))
+    print("   $2 = " + str(Reg[2]))
+    print("   $3 = " + str(Reg[3]))
+    print("   $4 = " + str(Reg[4]))
+    print("   $5 = " + str(Reg[5]))
+    print("   $6 = " + str(Reg[6]))
+    print("   $7 = " + str(Reg[7]))
     
 def main():
     i_memfile = open("i_mem.txt", "r")
     HexInstr = [] #array for instructions in hex
     BinInstr = [] #array for instructions in binary
+    Part2 = True if int(input("1 = Part 1\n2 = Part 1&2\n>>")) == 2 else False
     for l in i_memfile: #reading each line in txt file
         if (l == "\n" or l[0] == '#'): #skips over empty lines and comments
             continue
@@ -111,7 +129,7 @@ def main():
         l = format(int(l,16),"032b")
         BinInstr.append(l)
 
-    MIPSsim(HexInstr, BinInstr) #MIPSsim function
+    MIPSsim(HexInstr, BinInstr, Part2) #MIPSsim function
 
 if __name__ == "__main__": #NOT SURE HOW THIS WORKS BUT HERE IN CASE
     main()
